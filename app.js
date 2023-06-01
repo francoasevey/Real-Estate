9ab3a63e79cf3ddb63be917e06ab008ecd7957bf
francoromeo <francoromeo5012@gmail.com>
2023-06-01 19:47:18 -0300

    less -

---
+++ app.js
@@ -0,0 +1,16 @@
+let people = document.getElementById('people');
+let people_less = document.getElementById('people_less');
+let people_more = document.getElementById('people_more');
+
+let people_index = 4;
+
+people_less.addEventListener('click', () => {
+    people_index -= 1;
+
+    if(people_index <= 0){
+        people_index = 1;
+        people.value = people_index + ' People';
+    } else {
+        people.value = people_index + ' People';
+    }
+})
