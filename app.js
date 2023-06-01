3f4b0bca87ca2e892a62eddabd1ab6c65c9556b2
francoromeo <francoromeo5012@gmail.com>
2023-06-01 20:38:10 -0300

    checkin js

--- app.js
+++ app.js
@@ -25,3 +25,29 @@ people_more.addEventListener('click', () => {
         people.value = people_index + ' People';
     }
 })
+
+let checkout_data = document.getElementById('checkout_data');
+let checkout = document.getElementById('checkout');
+let checkin_data = document.getElementById('checkin_data');
+let checkin = document.getElementById('checkin');
+
+checkin.addEventListener('change', () => {
+    let checkin_value = checkin.value;
+    //console.log(checkin_value);
+
+    let dates = new Date(checkin_value);
+    let dates2 = new Date();
+    const week = ['Sun', 'Mon', 'Thu', 'Wed', 'Thu', 'Fri', 'Sat']
+    let day = week[dates.getDay()];
+
+    let date = checkin_value.slice(8, 10);
+
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+     let month = months[dates.getMonth()];
+
+     if (dates < dates2) {
+        console.log('No Date');
+     } else {
+        checkin_data.innerText = day + ', ' + date + ' ' + month;
+     }
+})
