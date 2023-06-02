e4f1aae06cbce682f626553b23df404b29ca9e86
francoromeo <francoromeo5012@gmail.com>
2023-06-01 21:40:47 -0300

    date fixit

--- app.js
+++ app.js
@@ -37,17 +37,39 @@ checkin.addEventListener('change', () => {
 
     let dates = new Date(checkin_value);
     let dates2 = new Date();
-    const week = ['Sun', 'Mon', 'Thu', 'Wed', 'Thu', 'Fri', 'Sat']
+    const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     let day = week[dates.getDay()];
 
     let date = checkin_value.slice(8, 10);
 
-    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-     let month = months[dates.getMonth()];
+    var monts = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+     let month = monts[dates.getMonth()];
 
      if (dates < dates2) {
         console.log('No Date');
      } else {
         checkin_data.innerText = day + ', ' + date + ' ' + month;
+     }
+})
+
+checkout.addEventListener('change', () => {
+    let checkout_value = checkout.value;
+    let checkin_value = checkin.value;
+    //console.log(checkin_value);
+
+    let dates = new Date(checkout_value);
+    let dates2 = new Date(checkin_value);
+    const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    let day = week[dates.getDay()];
+
+    let date = checkout_value.slice(8, 10);
+
+    let monts = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+     let month = monts[dates.getMonth()];
+
+     if (dates < dates2) {
+        console.log('No Date');
+     } else {
+        checkout_data.innerText = day + ', ' + date + ' ' + month;
      }
 })
